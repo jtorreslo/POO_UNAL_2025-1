18f67084c621927189f29ce1e321d472261f846b
jtorreslo <jtorreslo@unal.edu.co>
2025-05-20 20:11:05 -0500

    Update Cuenta.java

--- Ejercicios Actividad 3_2025_1/4_1Herencia/Cuenta.java
+++ Ejercicios Actividad 3_2025_1/4_1Herencia/Cuenta.java
@@ -1,1 +1,38 @@
+public class Cuenta {
+    protected float saldo;
+    
+    protected int numeroConsignaciones =0;
+    
+    protected int numeroRetiros = 0;
+    
+    protected float tasaAnual;
+    
+    protected float comisionAnual;
+    
+    protected float comisionMensual = 0;
+   
+
+    public Cuenta(float saldo, float tasaAnual){
+        this.saldo=saldo;
+        this.tasaAnual=tasaAnual;}
+
+    public void consignar(float cantidad){
+        saldo = saldo + cantidad;
+        numeroConsignaciones = numeroConsignaciones + 1;}
+    public void retirar(float cantidad){
+        float nuevoSaldo= saldo - cantidad;
+        if (nuevoSaldo>=0){
+            saldo-= cantidad;
+            numeroRetiros= numeroRetiros+1;}
+        else{
+            System.out.println("La cantidad a retirar excede el saldo actual");}}
+    
+    public void CalcularInteres(){
+        float tasaMesual = tasaAnual / 12;
+        float interesMesual =saldo * tasaMesual;
+        saldo+=interesMesual;}
+    public void extractoMesual(){
+        saldo-= comisionMensual;
+        CalcularInteres();}
+}
 
