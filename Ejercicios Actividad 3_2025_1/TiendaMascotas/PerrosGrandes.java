ff32f330e575e9f7a104d261454d680435064b6c
jtorreslo <jtorreslo@unal.edu.co>
2025-05-20 21:27:43 -0500

    Create PerrosGrandes.java

---
+++ Ejercicios Actividad 3_2025_1/TiendaMascotas/PerrosGrandes.java
@@ -0,0 +1,4 @@
+class PerrosGrandes extends Perro {
+    public PerrosGrandes(String nombre, int edad, String color, double peso, boolean muerde) {
+        super(nombre, edad, color, peso, muerde);
+    }}
