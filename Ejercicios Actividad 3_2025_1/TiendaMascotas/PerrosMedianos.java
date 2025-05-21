ebf305cbce73308e8a71f29fdcd75ae058b7bfb7
jtorreslo <jtorreslo@unal.edu.co>
2025-05-20 21:28:02 -0500

    Create PerrosMedianos.java

---
+++ Ejercicios Actividad 3_2025_1/TiendaMascotas/PerrosMedianos.java
@@ -0,0 +1,5 @@
+class PerrosMedianos extends Perro {
+    public PerrosMedianos(String nombre, int edad, String color, double peso, boolean muerde) {
+        super(nombre, edad, color, peso, muerde);
+    }
+}
