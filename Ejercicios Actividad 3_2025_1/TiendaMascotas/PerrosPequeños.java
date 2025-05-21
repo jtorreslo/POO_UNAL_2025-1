5cf54fdb46c9837a418f93cfb35f21b96b201d8c
jtorreslo <jtorreslo@unal.edu.co>
2025-05-20 21:28:22 -0500

    Create PerrosPequeños.java

---
+++ Ejercicios Actividad 3_2025_1/TiendaMascotas/PerrosPequeños.java
@@ -0,0 +1,5 @@
+class PerrosPequeños extends Perro {
+    public PerrosPequeños(String nombre, int edad, String color, double peso, boolean muerde) {
+        super(nombre, edad, color, peso, muerde);
+    }
+}
